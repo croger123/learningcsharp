0d15046bc7614eb2c094dd534015dcda0238c2ed
carole roger <caroleroger@hotmail.ca>
2023-01-30 18:56:40 -0500

    first commit i wanna push my code to github

---
+++ repos/HelloFromSharp/HelloFromSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Everybody!");
+Console.WriteLine("Please enter your name");
+string name = Console.ReadLine();
+
+NewMethod(name);
+Console.WriteLine("Please enter your last name: " );
+string lastName = Console.ReadLine();
+Console.WriteLine( "Hello " + name + " " + lastName);
+
+static void NewMethod(string name)
+{
+    Console.WriteLine("Hello " + name);
+}
